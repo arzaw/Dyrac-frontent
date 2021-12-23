a2c23cc15e2046c22d6d0b72f10a11be88a6c8da
arzaw <arzaw_11815115@nitkkr.ac.in>
2021-12-24 00:01:01 +0530

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LearningPathComponent } from './learning-path/learning-path.component';
-import { ContentDeatailComponent } from './content-deatail/content-deatail.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LearningPathComponent,
-    ContentDeatailComponent
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
